fb08928440fd27c8f89f3f15b7f45e6e89abcfdc
Daniel Gutiérrez <d.xander.gutierrez@gmail.com>
2024-08-23 07:44:54 -0600

    Sesion1

---
+++ Suma/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Suma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, suma;
+            Console.WriteLine("Ingrese un número entero: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro número entero: ");
+            num2 = int.Parse(Console.ReadLine());
+            suma = num1 + num2;
+            
+            Console.WriteLine($"La suma es {suma}");
+
+            
+
+        }   
+    }
+}
